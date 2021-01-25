15323754708f75723b5f5b2bf71a5ac3fc49ffba
joona127 <65039165+joona127@users.noreply.github.com>
2021-01-25 20:13:59 +0200

    Shoppinglist ready

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+const [input, setInput] = React.useState('');
+const [data, setData] = React.useState([]);
+
+const Add = () => {
+  setData([...data, {key: `${input}`}]);
+}
+
+const Clear = () => {
+  setData([]);
+
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
+      <TextInput
+
+        style={styles.TextInputStyle}
+
+        keyboardType={'default'}
+
+        onChangeText={(input) => setInput(input)}
+        value={input}
+
+      />
+
+<View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button title="ADD" onPress={Add} />
+        </View>
+        <View style={styles.button}>
+          <Button title="CLEAR" onPress={Clear} />
+        </View>
+      </View>
+      <Text style={styles.text}>Shopping List</Text>
+      <FlatList
+      data={data}
+      renderItem={({item}) =>
+      <Text>{item.key}</Text>}
+      />
     </View>
   );
 }
@@ -17,5 +51,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 170
   },
+  TextInputStyle: {
+    textAlign: 'center',
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    paddingTop: 40
+
+  },
+  button: {
+    width: '16%',
+    height: 40,
+    
+  },
+  text: {
+    paddingTop: 30,
+    fontWeight: 'bold',
+    fontSize: 17,
+    color: 'blue'
+  }
 });
